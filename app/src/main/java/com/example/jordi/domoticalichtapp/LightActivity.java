24db6085f1eeca453988db1bfb539abb4ab360dd
Jordi Bottema <Jordi Bottema>
2017-01-23 14:07:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jordi/domoticalichtapp/LightActivity.java
@@ -0,0 +1,13 @@
+package com.example.jordi.domoticalichtapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_light);
+    }
+}
