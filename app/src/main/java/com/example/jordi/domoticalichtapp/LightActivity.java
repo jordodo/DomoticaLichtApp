dcfad90a291dbad6ff4023854c378e96ba3233ea
jordodo <jordi.bottema@gmail.com>
2017-01-24 15:04:47 +0100

    Toggle Button is nu een switch, text op scherm verandert aan de van de status van de switch

--- app/src/main/java/com/example/jordi/domoticalichtapp/LightActivity.java
+++ app/src/main/java/com/example/jordi/domoticalichtapp/LightActivity.java
@@ -2,12 +2,44 @@ package com.example.jordi.domoticalichtapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class LightActivity extends AppCompatActivity {
+public class LightActivity extends AppCompatActivity
+{
+
+    Switch lichtSwitch;
+    TextView lichtText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light);
+
+        lichtSwitch = (Switch) findViewById(R.id.switch1);
+        lichtText = (TextView) findViewById(R.id.textView);
+
+        lichtSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if(isChecked)
+                {
+                    lichtText.setText("Licht staat aan");
+                }
+                else
+                {
+                    lichtText.setText("Licht staat uit");
+                }
+
+
+            }
+        });
+
+
     }
+
 }
